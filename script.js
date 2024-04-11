7ac216910fa13fc30607354e0f708768591b0483
Kundan Verma <85149300+Kundan1513@users.noreply.github.com>
2024-04-11 13:53:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// function toggleMenu() {
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+//   }
+
+function mylinkedin(){
+  window.open("https://www.linkedin.com/in/kundan-verma-a822b0213/");
+}
+function mygithub(){
+  window.open("https://github.com/Kundan1513");
+}
+function myjs(){
+  window.open("https://github.com/Kundan1513/JS-CAR-Game");
+}
+function myac(){
+  window.open("https://github.com/Kundan1513/Amazon_Clone");
+}
+function mypdd(){
+  window.open("https://github.com/Kundan1513/Parkinson-s-Disease-Detection");
+}
